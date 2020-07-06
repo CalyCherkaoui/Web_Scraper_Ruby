55adb880f5fe23059754aa773e24d74e774fdee6
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-07-06 15:05:25 -0500

    Parsing web page using HTTParty and Nokogiri

--- bin/main.rb
+++ bin/main.rb
@@ -2,3 +2,13 @@ require 'nokogiri'
 require 'httparty'
 require 'byebug'
 require 'colorize'
+
+def scraper
+  url = "https://www.careerbeacon.com/en/search"
+  unparsed_page = HTTParty.get(url) #raw html
+  parsed_page = NOKOGIRI::HTML(unparsed_page) #parse html
+  byebug
+
+end
+
+scraper
