18c24de2d2d082555ad580baaf8d9d4d47628831
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-07-06 18:59:30 -0500

    updating Scraper class with get_all_offers methods

--- lib/page.rb
+++ lib/page.rb
@@ -8,18 +8,13 @@ class Page
   def initialize(url)
     @url = url
   end
-  #private
+
   def get_html
     document = open(@url)
     @raw_html = document.read #raw html
     @raw_html
   end
-  public
-  def parse_html
-    @parsed_html = Nokogiri::HTML(get_html) #parse html
-  end
+  
 end
 
-pg = Page.new("https://www.careerbeacon.com/en/search")
-x = pg.parse_html
-p x
+
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,23 @@
+require_relative 'page.rb'
+require 'nokogiri'
+require 'byebug'
+require 'colorize'
+require 'open-uri'
+
 class Scraper
   attr_accessor :page
-  def initilize(page)
-    @page = page
+  def initialize(page)
+    @page = Nokogiri::HTML(page.get_html) #parse html
   end
+
+  def get_all_offers
+  job_offers = @page.css('div.jobsearch-SerpJobCard')
+  job_offers
+  end
+
 end
+ 
+pg = Page.new("https://www.indeed.com/jobs?q=developer+remote&l=")
+x = Scraper.new(pg)
+y = x.get_all_offers
+p y.count
