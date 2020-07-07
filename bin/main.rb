7bc3d2ef6eda51c0d03df8a412af2a0bcc448cfc
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-07-07 16:28:14 -0500

    Fixing the display time in terminal using sleep method

--- bin/main.rb
+++ bin/main.rb
@@ -22,18 +22,17 @@ option = gets.chomp.to_i
 web_page = Page.new('https://www.indeed.com/jobs?q=developer+remote&l=')
 my_scraper = Scraper.new(web_page)
 count = my_scraper.jobs_list.count
-
+sleep 0.3
+puts user_interface.after_option_msg(option)
+sleep 3
 if option == 1
   i = 0
   while i < count
     puts my_scraper.display_it(i)
     i += 1
   end
-
 elsif option == 2
   my_scraper.make_csv
 elsif option == 3
   my_scraper.make_txt
-else
-  puts 'Wrong option try again'
 end
