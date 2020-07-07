69ba36a1c1be03c3cd428fe689289ebf97561367
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-07-06 19:15:30 -0500

    Adding Job and Job_listing methods to Scraper class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,14 +10,31 @@ class Scraper
     @page = Nokogiri::HTML(page.get_html) #parse html
   end
 
-  def get_all_offers
-  job_offers = @page.css('div.jobsearch-SerpJobCard')
-  job_offers
+  def job(entry)
+    job = {
+      title: entry.css().text,
+      location: entry.css().text,
+      date_publication: entry.css().text,
+      company: entry.css().text,
+      salary: entry.css().text,
+      summary: entry.css().text,
+      url: entry.css().text
+    }
   end
 
+  def job_listing
+    jobs = []
+    job_offers = @page.css('div.jobsearch-SerpJobCard')
+    job_offers.each do |entry|
+      job = job(entry)
+      jobs << job
+    end
+    jobs
+  end
+    
 end
  
 pg = Page.new("https://www.indeed.com/jobs?q=developer+remote&l=")
 x = Scraper.new(pg)
-y = x.get_all_offers
+y = x.all_offers
 p y.count
