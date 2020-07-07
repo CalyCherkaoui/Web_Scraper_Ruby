58369fde93cb4e3277d6fc801448a0ebd198ea05
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-07-07 13:43:31 -0500

    Creating a string filter method to filter the scraped texts

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,19 +9,28 @@ class Scraper
   def initialize(page)
     @page = Nokogiri::HTML(page.get_html) #parse html
   end
-
+  private
   def job(entry)
     job = {
-      title: entry.css('h2.title').text,
-      # location: entry.css().text,
-      # date_publication: entry.css().text,
-      # company: entry.css().text,
-      # salary: entry.css().text,
-      # summary: entry.css().text,
-      # url: entry.css().text
+      title: string_filter(entry.css('.title').text),
+      location: string_filter(entry.css('.location .accessible-contrast-color-location').text),
+      date_publication: string_filter(entry.css('span.date').text),
+      company: string_filter(entry.css('.company').text),
+      summary: string_filter(entry.css('div.summary').text),
+      url: entry.css('a.jobtitle')[0].attributes['href'].value
     }
   end
-
+  def string_filter(my_string)
+    if my_string.empty?
+      str = 'Data not Availible'
+    else
+      output = my_string.gsub(/[\n\r\t]/, '')
+      output = output.delete_prefix("  ")
+      output = output.delete_suffix("new")
+      output
+    end
+  end
+  public
   def job_listing
     jobs = []
     job_offers = @page.css('div.jobsearch-SerpJobCard')
@@ -38,4 +47,15 @@ pg = Page.new("https://www.indeed.com/jobs?q=developer+remote&l=")
 x = Scraper.new(pg)
 y = x.job_listing
 p y.count
-p y[0]
+z = y[0][:title]
+p z
+f =y[0][:company]
+p f
+g =y[0][:location]
+p g
+h = y[0][:date_publication]
+p h
+h = y[0][:summary]
+p h
+h = y[0][:url]
+p h
