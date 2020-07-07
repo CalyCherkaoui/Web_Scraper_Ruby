ff4a086cdc8b6342d5c0d7d5b16a63e71cad851b
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-07-07 10:29:52 -0500

    adding make_csv and make_txt methods to Output class

--- bin/main.rb
+++ bin/main.rb
@@ -4,8 +4,8 @@ require 'colorize'
 require 'open-uri'
 
 def scraper
-  # url = "https://www.careerbeacon.com/en/search"
-  url = "https://www.indeed.com/jobs?q=developer+remote&l="
+  url = "https://www.careerbeacon.com/en/search"
+  # url = "https://www.indeed.com/jobs?q=developer+remote&l="
   document = open(url)
   raw_html = document.read #raw html
   parsed_html = Nokogiri::HTML(raw_html) #parse html
@@ -15,10 +15,10 @@ def scraper
   jobs = []
   jobs_list.each do |entry|
     job = {
-      title: entry.css('strong').text,
-      location: entry.css('span.location').text,
-      publication_date: entry.css('div.job_pub_date').text,
-      company: entry.css('div.job_company').text
+      title: entry.css('strong').inner_text,
+      location: entry.css('span.location').inner_text,
+      publication_date: entry.css('div.job_pub_date').inner_text,
+      company: entry.css('div.job_company').inner_text
       # url:
     }
     jobs << job
--- lib/output.rb
+++ lib/output.rb
@@ -1,3 +1,4 @@
+require 'csv'
 class Output
   attr_accessor :jobs_list
   def initialize (scraper)
@@ -5,7 +6,14 @@ class Output
   end
 
   def make_cvs
-
+    filepath = "test.csv"
+    csv_option = {headers: :first_row, col_sep: ','}
+    CSV.open(filepath, 'wb', csv_option) do |csv|
+      csv << ['title']
+      @jobs_list.each do |elem|
+        csv << [elem]
+      end
+    end
   end
 
   def make_JSON
@@ -13,6 +21,10 @@ class Output
   end
 
   def make_txt
+    filepath = "test.txt"
+    txt_file = File.open(filepath, 'w')
+    txt_file.write(@jobs_list)
+    txt_file.close()
   
   end
 
